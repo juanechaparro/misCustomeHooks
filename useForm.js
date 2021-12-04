684ac203c2dbbbbae47a769cee2a667878dabf02
JUAN ENRIQUE CHAPARRO GUARNIZO <juanechaparro@hotmail.com>
2021-12-04 14:53:57 -0500

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,17 @@
+
+import { useState } from "react"
+export const useForm = (initialState = {}) => {
+    const [values, setValues] = useState(initialState)
+    const reset = ()=>{
+        setValues(initialState);
+    }
+    // se puede enviar como segunfo arrat directamente
+    const handleInputChange = ({target}) =>{
+        setValues({
+            ...values,
+            [target.name] :target.value
+        })
+    }
+
+    return[values, handleInputChange,reset];
+}
